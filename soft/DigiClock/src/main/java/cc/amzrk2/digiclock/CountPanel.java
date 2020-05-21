d5e6ca3d5ef1766cb069e21fdf6ecce65a7fcb25
8f235831 <49898931+8f235831@users.noreply.github.com>
2020-05-21 22:30:41 +0800

    Update CountPanel.java

--- soft/DigiClock/src/main/java/cc/amzrk2/digiclock/CountPanel.java
+++ soft/DigiClock/src/main/java/cc/amzrk2/digiclock/CountPanel.java
@@ -1,19 +1,40 @@
 package cc.amzrk2.digiclock;
-import javax.swing.*;
-import java.awt.*;
 
-public class CountPanel extends JPanel
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+/**
+ * 倒计时对应的窗口组件类。继承自<code>javax.swing.JPanel</code>类。
+ *
+ * @author 8f235831
+ * @see CountCheck
+ */
+public class CountPanel extends JPanel implements Comparable<CountPanel>
 {
-	private int hashCode;
-	public JLabel content_1;
-	public JLabel content_2;
-	public JButton deleteButton;
+	private int hashCode;// 哈希值，与对应的倒计时类保持一致。
+	public JLabel content_1;// 文本，用于显示具体时间。
+	public JLabel content_2;// 文本，用于显示预计时间。
+	public JButton deleteButton; // 删除按钮。
 
+	/**
+	 * 构造方法，使用哈希函数生成。不会实际构造，仅供比较使用。
+	 *
+	 * @param hashCode 需要的哈希值。
+	 */
 	private CountPanel(int hashCode)
 	{
 		this.hashCode = hashCode;
 	}
 
+	/**
+	 * 构造方法，构造一个窗口组件。
+	 *
+	 * @param timeManager 管理线程类。
+	 */
 	public CountPanel(TimeManager timeManager)
 	{
 		super();
@@ -31,9 +52,9 @@ public class CountPanel extends JPanel
 		this.content_2 = new JLabel("content2");
 		this.deleteButton = new JButton("删除");
 
-                this.content_1.setFont(DigiClock.fontSm);
-                this.content_2.setFont(DigiClock.fontSm);
-                this.deleteButton.setFont(DigiClock.fontSm);
+		this.content_1.setFont(DigiClock.fontSm);
+		this.content_2.setFont(DigiClock.fontSm);
+		this.deleteButton.setFont(DigiClock.fontSm);
 
 		this.deleteButton.addActionListener(
 				event -> timeManager.removeCount(this.hashCode)
@@ -64,11 +85,20 @@ public class CountPanel extends JPanel
 		// this.setVisible(true);
 	}
 
+	/**
+	 * 设置哈希值。
+	 *
+	 * @param hashCode 哈希值，应当由对应的闹钟时间生成。
+	 */
 	public void setHashCode(int hashCode)
 	{
 		this.hashCode = hashCode;
 	}
 
+	/**
+	 * 这个方法规定了窗口组件的大小。<br/>
+	 * <big><strong>不用碰这个方法。</strong></big>
+	 */
 	@Override
 	public Dimension getPreferredSize()
 	{
@@ -76,20 +106,48 @@ public class CountPanel extends JPanel
 		return new Dimension(180, 70);
 	}
 
+	/**
+	 * 根据给定的哈希值，生成一个用于比较的实例。该实例不能用于嵌入窗口中显示。
+	 *
+	 * @param hashCode 给定的哈希值。
+	 * @return 返回一个实例。
+	 */
 	public static CountPanel getInstanceCompare(int hashCode)
 	{
 		return new CountPanel(hashCode);
 	}
 
-        @Override
-        public int hashCode()
-        {
-            return this.hashCode;
-        }
-        
-        @Override
-        public boolean equals(Object obj)
-        {
-            return (obj instanceof CountPanel)&& this.hashCode == obj.hashCode();
-        }
+	/**
+	 * 返回设定的哈希值。
+	 *
+	 * @return 设定的哈希值。
+	 */
+	@Override
+	public int hashCode()
+	{
+		return this.hashCode;
+	}
+
+	/**
+	 * 比较两个窗口是否相同。
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		return (obj instanceof CountPanel) && this.hashCode == obj.hashCode();
+	}
+
+	/**
+	 * 比较方法，比较哪一个组件更早。
+	 *
+	 * @param o 另一个组件。
+	 * @return 这个倒计时结束的时间较早，返回<code> 1 </code>；
+	 * 这个闹倒计时结束的时间较晚，返回<code> -1 </code>；
+	 * 结束时间相同，返回<code> 0 <code/>。
+	 */
+	@Override
+	public int compareTo(CountPanel o)
+	{
+		return -(Integer.compare(this.hashCode, o.hashCode));
+	}
 }
